6a958a5ef453ecb422cf0a3f2fb26a7b3e689da4
Neil Seejoor <nvseejoor@gmail.com>
2018-07-16 19:40:26 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,6 @@ class PostsController < ApplicationController
 	  @post.title = params[:title]
 	  @post.description = params[:description]
 	  @post.save
-	  redirect_to new_post_path(@post)
+	  redirect_to new_post_path
 	end
 end
